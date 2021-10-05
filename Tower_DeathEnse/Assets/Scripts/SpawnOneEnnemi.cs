468f09927fd39906f9182bf8a8d4106ea90318d7
BaptistErr <baptisteherr@aol.com>
2021-10-05 17:00:20 +0200

    test git

---
+++ Tower_DeathEnse/Assets/Scripts/SpawnOneEnnemi.cs
@@ -0,0 +1,23 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOneEnnemi : MonoBehaviour
+{
+    
+   
+    void Start()
+    {
+        public string NomEnnemi;
+        public Transform Ennemi;
+        public int NombreEnnemi;
+        public float Frequence;
+    }
+    void Update()
+{
+
+}
+    
+    
+}
+*/

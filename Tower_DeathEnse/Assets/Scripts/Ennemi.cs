e7cd9a4fca71d203086fb34c8d25665d17dfe059
BaptistErr <baptisteherr@aol.com>
2021-10-05 17:34:29 +0200

    ye comprend pas

---
+++ Tower_DeathEnse/Assets/Scripts/Ennemi.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ennemi : MonoBehaviour
+{
+    //caracteristique de l'ennemi
+
+    public int health;
+    public int damage;
+    //---------------------------
+
+    //mouvement de l'ennemi
+
+    public Transform Target;
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent.SetDestination(Target.transform.position);
+    }
+    //-----------------------------
+
+    //methodes de l'ennemi
+    private void DealDamage()
+    {
+
+    }
+    private bool isDead()
+    {
+        if (health <= 0)
+        {
+            return true;
+        }
+        else return false;
+    }
+}

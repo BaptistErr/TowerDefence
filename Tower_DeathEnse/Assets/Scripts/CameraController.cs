13729dd04d9d6878e359b3f621523673dd124254
BaptistErr <baptisteherr@aol.com>
2021-10-05 18:08:13 +0200

    Raycast tower

--- Tower_DeathEnse/Assets/Scripts/CameraController.cs
+++ Tower_DeathEnse/Assets/Scripts/CameraController.cs
@@ -6,6 +6,8 @@ public class CameraController : MonoBehaviour
 {
     public float speed;
 
+    public Object tower;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +20,16 @@ public class CameraController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         transform.position += new Vector3(horizontal, 0, vertical) * Time.deltaTime * speed;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawLine(ray.origin, hit.point, Color.red, 100000f);
+                Instantiate(tower, hit.point, Quaternion.identity);
+            }
+        }
     }
 }

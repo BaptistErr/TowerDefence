5a49abde2c6eb2cff7b92f6b392bc6d5cd80f99c
BaptistErr <baptisteherr@aol.com>
2021-10-05 15:34:57 +0200

    LA TOUR TOURNE

--- Tower_DeathEnse/Assets/Scripts/TowerBehaviour.cs
+++ Tower_DeathEnse/Assets/Scripts/TowerBehaviour.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TowerBehaviour : MonoBehaviour
 {
+    private SphereCollider sphere;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sphere = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Vector3 target = new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z);
+        transform.LookAt(target);
     }
 }

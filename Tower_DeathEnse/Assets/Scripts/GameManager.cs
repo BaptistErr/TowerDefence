cc2bb272a30f81e046bc88fc3f0d3702d2c0a50b
BaptistErr <baptisteherr@aol.com>
2021-10-07 10:25:21 +0200

    advancement

--- Tower_DeathEnse/Assets/Scripts/GameManager.cs
+++ Tower_DeathEnse/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,7 +12,6 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame

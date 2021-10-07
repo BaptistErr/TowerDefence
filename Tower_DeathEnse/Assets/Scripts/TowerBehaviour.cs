b32df9a57db6080245e01a6fbf61affb9d351696
BaptistErr <baptisteherr@aol.com>
2021-10-07 11:30:34 +0200

    tower shoot

--- Tower_DeathEnse/Assets/Scripts/TowerBehaviour.cs
+++ Tower_DeathEnse/Assets/Scripts/TowerBehaviour.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerBehaviour : MonoBehaviour
 {
+    public GameObject bullet;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,14 @@ public class TowerBehaviour : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
+        Debug.Log("yo");
         Vector3 target = new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z);
         transform.LookAt(target);
+        InvokeRepeating("Shoot", 0, 0);
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
     }
 }

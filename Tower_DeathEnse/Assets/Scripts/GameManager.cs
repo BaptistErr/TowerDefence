8026f3b0af5610cf7c0eb25acb4807c07ae29cd6
hmarseu <hmarseu@gaming.tech>
2021-10-08 13:01:46 +0200

    grosse maj

--- Tower_DeathEnse/Assets/Scripts/GameManager.cs
+++ Tower_DeathEnse/Assets/Scripts/GameManager.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class GameManager : MonoBehaviour
 {
     public Object tower;
+   
+    
 
     private List<Collider> slotsOccupied = new List<Collider>();
 
     // Start is called before the first frame update
     void Start()
     {
+       
+     
     }
 
     // Update is called once per frame
--- Tower_DeathEnse/Assets/Scripts/SpawnEnnemi.cs
+++ Tower_DeathEnse/Assets/Scripts/SpawnEnnemi.cs
@@ -2,29 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
- 
+
+
 public class SpawnEnnemi : MonoBehaviour
 {
+    //déclaration du NavMesh
     public Transform target;
     public GameObject Spawnee;
     public NavMeshAgent agent;
+
+    //déclaration des vagues d'énnemis
     public bool StopSpawn = false;
-    public float tpsSpawn;
-    public float delaiSpawn;
-    
+    private int nombreEnnemi ;
+    private int nombreMax =5;
+    int NbVague = 6;
+
     void Start()
     {
         agent.SetDestination(target.transform.position);
-        InvokeRepeating("SpawnObjet", tpsSpawn, delaiSpawn);
+        jeux();
     }
+
+
+    //fct -> lancer les vagues d'ennemis 
+    public void Wave(int vitesseennemi, int TpsSpawn, int DelaySpawn)
+
+    {
+        agent.speed = vitesseennemi;
+        InvokeRepeating("SpawnObjet", TpsSpawn, DelaySpawn);
+    }
+    //fct -> gère le nbr d'ennemis 
     public void SpawnObjet()
     {
-        Instantiate(Spawnee, transform.position, transform.rotation);
-        if (StopSpawn)
+        if ( nombreEnnemi < nombreMax)
         {
+            nombreEnnemi += 1;
+            Instantiate(Spawnee, transform.position, transform.rotation);
+
+        }
+        else
+        {
+            nombreEnnemi = 0;
+            nombreMax += 2;
             CancelInvoke("SpawnObjet");
+           
         }
     }
-    
 
+    //fct -> réguler les vagues 
+    public IEnumerator ReguVague(float TpsEntreVague)
+    {
+        while (true)
+        {
+            Wave(2, 2, 2);
+            yield return new WaitForSeconds(TpsEntreVague);
+        }
+    }
+
+    
+    public void jeux()
+    {
+        //commencer la coroutine 
+        StartCoroutine(ReguVague(5));
+       //arreter la coroutine a changer d'endroit
+        StopCoroutine(ReguVague(5));
+    }
 }
